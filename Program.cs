2b81419b6c144d440bc91580fceaecca8bba8f97
DanaSadeh <47765070+DanaSadeh@users.noreply.github.com>
2019-02-19 19:29:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("write a number");
+            a=Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("write another number");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a>b)
+            {
+                Console.WriteLine(a+ " is bigger than " +b);
+            
+            }
+            else
+            {
+                Console.WriteLine(b + " is bigger than " + a);
+            }
+        }
+    }
+}
